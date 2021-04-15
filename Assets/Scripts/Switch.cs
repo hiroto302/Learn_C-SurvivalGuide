ec57c72c3c6e37ab8784bc08209ae7d3a6796d85
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-15 19:57:37 +0900

    Create Switch
    Random.Range , KeyCode.Alpha0

---
+++ Assets/Scripts/Switch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Switch : MonoBehaviour
+{
+    [SerializeField]
+    int point;
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Alpha0))
+        {
+            point = Random.Range(0, 3);
+
+            switch(point)
+            {
+                case 0:
+                    Debug.Log("bad");
+                    break;
+                case 1:
+                    Debug.Log("normal");
+                    break;
+                default:
+                    Debug.Log("good");
+                    break;
+            }
+        }
+
+    Debug.Log(point);
+
+
+
+    }
+}
