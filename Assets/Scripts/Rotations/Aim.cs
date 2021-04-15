eb28aad5ce3fa0de499836316fe3f2264c20bb4d
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-15 14:54:51 +0900

    Quaternion.LookRotation

---
+++ Assets/Scripts/Rotations/Aim.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Aim : MonoBehaviour
+{
+    [SerializeField]
+    Transform _target;
+
+    void Update()
+    {
+        // direction = destination - source;
+        Vector3 directionToFace = _target.position - transform.position;
+        // Draws a Line SceneView
+        Debug.DrawRay(transform.position, directionToFace, Color.green);
+        // access our current rotation = Quaternion Look Rotation
+        transform.rotation = Quaternion.LookRotation(directionToFace);
+        // make the rotation smooth
+    }
+}
