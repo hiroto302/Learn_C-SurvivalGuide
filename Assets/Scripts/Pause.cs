e4fe8260677817429097f530f4091f0b6768aa31
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-15 09:39:15 +0900

    Time.timeScale

---
+++ Assets/Scripts/Pause.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pause : MonoBehaviour
+{
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        // 完全停止
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Time.timeScale = 0;
+        }
+
+        if (Input.GetKeyUp(KeyCode.Space))
+        {
+            Time.timeScale = 1.0f;
+        }
+
+        // 2倍速
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Time.timeScale = 2.0f;
+        }
+
+        if (Input.GetKeyUp(KeyCode.R))
+        {
+            Time.timeScale = 1.0f;
+        }
+    }
+}
