db6028cec4252669105cd601583f771c934c0c20
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-15 23:04:45 +0900

    Return Array
    Random.value

---
+++ Assets/Scripts/Loops/Arrays/ReturnArrayMethod.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ReturnArrayMethod : MonoBehaviour
+{
+    public GameObject[] players;
+    void Start()
+    {
+        players = GetAllPlayers();
+    }
+    GameObject[] GetAllPlayers()
+    {
+        GameObject[] allPlayers = GameObject.FindGameObjectsWithTag("Player");
+        foreach(var p in allPlayers)
+        {
+            p.GetComponent<Renderer>().material.color = new Color(Random.value, Random.value, Random.value);
+        }
+        return allPlayers;
+    }
+}
