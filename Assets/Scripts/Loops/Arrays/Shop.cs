09ee9e8d9f622040adcf08a893dd870d39dc5b28
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-15 22:37:59 +0900

    [System.Serializable]

---
+++ Assets/Scripts/Loops/Arrays/Shop.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// 品物 : Serializable 属性によりインスペクター上でクラスにサブプロパティーを埋め込むことを許可する
+[System.Serializable]
+public class Item
+{
+    public int itemID;
+    public string itemName;
+    public string itemDescription;
+}
+// お店にある品物を管理するクラス
+public class Shop : MonoBehaviour
+{
+    public Item[] storedItems;
+    // Start is called before the first frame update
+    void Start()
+    {
+        foreach(var item in storedItems)
+        {
+            Debug.Log(item.itemDescription);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
