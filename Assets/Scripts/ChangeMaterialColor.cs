ef5fb68705b96149733d13c45bd86af75b527e09
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-15 17:29:47 +0900

    material.colorの変更

---
+++ Assets/Scripts/ChangeMaterialColor.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialColor : MonoBehaviour
+{
+    public GameObject player;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            player.GetComponent<Renderer>().material.color = Color.black;
+        }
+    }
+}
