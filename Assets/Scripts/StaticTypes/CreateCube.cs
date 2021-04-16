9151d639376430fc5774156126a47db4fbdc645d
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-16 22:49:25 +0900

    Utility(Static) クラスの活用方法について

---
+++ Assets/Scripts/StaticTypes/CreateCube.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using CreateObject;
+
+public class CreateCube : MonoBehaviour
+{
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.C))
+        {
+            UtilityHelper.CreateObject();
+        }
+        if(Input.GetKeyDown(KeyCode.S))
+        {
+            UtilityHelper.SetPositionZero(this.gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/StaticTypes/UtilityHelper.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CreateObject
+{
+    public static class UtilityHelper
+    {
+        public static void CreateObject()
+        {
+            // Create a new primitive cube
+            GameObject.CreatePrimitive(PrimitiveType.Cube);
+        }
+        public static void SetPositionZero(GameObject obj)
+        {
+            obj.transform.position = Vector3.zero;
+        }
+    }
+}
