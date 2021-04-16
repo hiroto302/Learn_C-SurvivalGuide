60e7bbf289834bab36dab7a3ba4984747c260ff3
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 07:46:46 +0900

    enum の index 利用法について

---
+++ Assets/Scripts/Enums/SelectedDifficulty.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+// 画面の難易度選択によって、ロードする画面を決定することを想定したクラス作成
+public class SelectedDifficulty : MonoBehaviour
+{
+    public enum Difficult
+    {
+        Easy,       // 0 デフォルト
+        Normal,     // 1
+        Hard = 5,       // 2
+        Expert = 0      // 3  ExpertのインデックスをEasyと被らせると、Easyの扱いになる
+    }
+
+    public Difficult selectedDifficulty;
+
+
+    public void Update()
+    {
+        Debug.Log((int)selectedDifficulty);
+    }
+
+    public void LoadScene()
+    {
+        SceneManager.LoadScene((int)selectedDifficulty);
+    }
+}
