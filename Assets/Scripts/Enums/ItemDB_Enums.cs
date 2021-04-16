d60907e3fd817b428377b28c18be223986ce443c
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 07:25:40 +0900

    enum を  利用したItemDBの機能作成

---
+++ Assets/Scripts/Enums/ItemDB_Enums.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB_Enums : MonoBehaviour
+{
+    public List<Item_Enums> itemDB = new List<Item_Enums>();
+
+    void Update()
+    {
+        itemDB[0].Action();
+    }
+}
---
+++ Assets/Scripts/Enums/Item_Enums.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item_Enums
+{
+    public string name;
+    public int id;
+
+    public enum ItemType
+    {
+        None,
+        Weapon,
+        Consumable
+    }
+
+    public ItemType itemType;
+
+    public void Action()
+    {
+        switch(itemType)
+        {
+            case Item_Enums.ItemType.Weapon:
+                Debug.Log("This is a Weapon");
+                break;
+            case ItemType.Consumable:
+                Debug.Log("This is a Consumable");
+                break;
+        }
+    }
+}
