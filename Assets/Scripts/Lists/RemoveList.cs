0d4ab70be64989b2ab54e8cabfa789d516648152
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 03:16:35 +0900

    List Remove, RemoveAt

---
+++ Assets/Scripts/Lists/RemoveList.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveList : MonoBehaviour
+{
+    public List<string> names = new List<string>();
+
+    void Start()
+    {
+        names.Add("a");
+        names.Add("b");
+        names.Add("c");
+        names.Add("d");
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            names.RemoveAt(Random.Range(0, names.Count));
+        }
+        if(Input.GetKeyDown(KeyCode.E))
+        {
+            var nameToRemove = names[Random.Range(0, names.Count)];
+            names.Remove(nameToRemove);
+        }
+    }
+
+}
