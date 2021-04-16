d5d2c14ba611ec8639d188f0d3cd841c06dc3e12
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 02:02:50 +0900

    Propertyについて

---
+++ Assets/Scripts/StaticTypes/Properties.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class Enemy : MonoBehaviour
+{
+    Properties player;
+    public void Start()
+    {
+        player = new Properties();
+    }
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            Attack(player);
+        }
+    }
+    void Attack(Properties player)
+    {
+        // 敵は相手を倒すことができる
+        player.IsDead = true;
+        Debug.Log("倒したよ");
+    }
+    void PlayerRecovery()
+    {
+        // isRevivedを読み取り専用にすることで、敵は相手を回復させることはできないようにする
+        // Properties.isRevived = true;
+    }
+}
+public class Properties : MonoBehaviour
+{
+    private bool isDead;
+    // Auto Properties
+    // ReadOnly
+    static public bool isRevived {private set; get; }
+
+    public bool IsDead
+    {
+        set
+        {
+            if(value == true)
+            {
+                Debug.Log("Player id Dead!!");
+            }
+            isDead = value;
+        }
+        get {return isDead;}
+    }
+
+    public void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.D))
+        {
+            IsDead = true;
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            isRevived = true;
+        }
+    }
+}
