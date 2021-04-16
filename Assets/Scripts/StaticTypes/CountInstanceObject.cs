0708f361fe5c8f20c1f10f66123ed568edae3d48
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-16 21:43:16 +0900

    Static Type について

---
+++ Assets/Scripts/StaticTypes/CountInstanceObject.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class Object
+{
+    public int id;
+    public static int ObjectInstanceNum;
+
+    public Object()
+    {
+        ObjectInstanceNum++;
+    }
+}
+
+public class CountInstanceObject : MonoBehaviour
+{
+    void Start()
+    {
+        Object o1 = new Object();
+        Object o2 = new Object();
+        Object o3 = new Object();
+        Debug.Log(Object.ObjectInstanceNum);
+    }
+}
+
+
