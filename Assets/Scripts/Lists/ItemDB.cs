c1262d9c92ada4427c2d2e885ec2b9aa37b5db87
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 07:01:31 +0900

    DB の　基礎機能

---
+++ Assets/Scripts/Lists/ItemDB.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+// using PlayerItem;
+
+public class ItemDB : MonoBehaviour
+{
+
+    public List<PlayerItem.Item> itemDatabase = new List<PlayerItem.Item>();
+    public List<Item> itemDatabase2 = new List<Item>();
+
+    public void AddItem(int itemID, Player_Lists player)
+    {
+        foreach(var item in itemDatabase)
+        {
+            if(item.id == itemID)
+            {
+                Debug.Log("we have a match");
+                player.inventory[0] = item;
+                return;
+            }
+        }
+        Debug.Log("Item dose not exist");
+    }
+    public void RemoveItem(int itemID, Player_Lists player)
+    {
+        foreach(var item in itemDatabase)
+        {
+            if(item.id == itemID)
+            {
+                player.inventory[0] = null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Lists/Player_Lists.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Lists : MonoBehaviour
+{
+    public PlayerItem.Item[] inventory = new PlayerItem.Item[10];
+    ItemDB _ItemDB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _ItemDB = GameObject.Find("God").GetComponent<ItemDB>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.A))
+        {
+            // only this means this Script class
+            _ItemDB.AddItem(0, this);
+        }
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            _ItemDB.RemoveItem(0, this);
+        }
+    }
+}
