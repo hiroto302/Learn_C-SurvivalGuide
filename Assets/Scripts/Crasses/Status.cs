e3ce20f7b4ff37ab27093fc0747e73f362582b7f
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-16 16:52:45 +0900

    Create StatusList.cs
    [System.Serializable]の活用方法の模索

---
+++ Assets/Scripts/Crasses/Status.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Status
+{
+    public string name;
+    public int id;
+    public string description;
+
+    public Status(string name, int id, string description)
+    {
+        this.name = name;
+        this.id = id;
+        this.description = description;
+    }
+}
---
+++ Assets/Scripts/Crasses/StatusList.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StatusList : MonoBehaviour
+{
+    public Status Luck;
+    public Status Strong;
+
+    void Start()
+    {
+        Luck = new Status("Luck", 1, "運");
+        Strong = CreateStatus("Strong", 2, "腕力");
+    }
+
+    Status CreateStatus(string name, int id, string description)
+    {
+        var status = new Status(name, id ,description);
+        return status;
+    }
+}
