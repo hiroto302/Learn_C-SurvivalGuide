6918ee769ec2f1c179e8fb6e2b6bee86db936baa
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-16 21:14:57 +0900

    継承とSystem.Serializableの活用方法

---
+++ Assets/Scripts/Crasses/Human_normal.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Human_normal : Human
+{
+  public string description = "普通の人間";
+}
---
+++ Assets/Scripts/Crasses/PlayerList.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerList : MonoBehaviour
+{
+    public Human_normal human_normal = new Human_normal();
+    public Human_wizard human_wizard = new Human_wizard();
+}
