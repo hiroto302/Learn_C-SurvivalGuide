89839096626c4563bcc684cf184565c20b667fc8
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-16 23:42:40 +0900

    Static Constructor について

---
+++ Assets/Scripts/StaticTypes/StaticConstructorTest.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple
+{
+    public string type;
+    public string taste;
+
+    public static string locality;
+
+    public Apple()
+    {
+        Debug.Log("りんご");
+    }
+    static Apple()
+    {
+        locality = "青森";
+        Debug.Log("りんごの産地 : " + locality);
+    }
+}
+public class StaticConstructorTest : MonoBehaviour
+{
+    public void Start()
+    {
+        Apple a1 = new Apple();
+        var a2 = new Apple();
+        var a3 = new Apple();
+    }
+}
