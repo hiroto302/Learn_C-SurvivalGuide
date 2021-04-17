aaf53612838a8371670d2be9cbeaaefc36aede0b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 03:13:03 +0900

    Practicing1
    C# Delegates with and without Return Types and Parameters

---
+++ Assets/Scripts/Delegates/Delegates_P1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using UnityEngine;
+
+// Practicing_1
+public class Delegates_P1 : MonoBehaviour
+{
+    public Action<int, int> Sum;
+    public Action<int, int> Sum2;
+
+    public void Start()
+    {
+        // Delegate
+        Sum = CalculationSum;
+        Sum(1,2);
+
+        // Deleget + Lambda
+        Sum2 = (a, b) => {
+            var total = a + b;
+            if(total < 100)
+            {
+                Debug.Log("Total is less than 100");
+            }
+            Debug.Log("Sum2 : " + total);
+        };
+        Sum2(1, 7);
+    }
+
+    void CalculationSum(int a, int b)
+    {
+        var total = a + b;
+        Debug.Log("Sum : " + total);
+    }
+}
