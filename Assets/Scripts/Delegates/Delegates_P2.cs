21e0e2eb80ebc49a28f0642e51c8e4128b378450
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 03:13:45 +0900

    Practicing2

---
+++ Assets/Scripts/Delegates/Delegates_P2.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+// Practicing_2
+public class Delegates_P2 : MonoBehaviour
+{
+    public Action onGetName;
+
+    void Start()
+    {
+        onGetName = () => Debug.Log("GameObjectName : " + gameObject.name);
+        onGetName();
+    }
+}
