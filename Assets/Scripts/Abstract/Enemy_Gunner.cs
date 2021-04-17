94b9f8acab2f797f0a215e420337cee469c233e6
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 18:28:45 +0900

    Learn Polymorphism
    インターフェースを使用すること利点について

--- Assets/Scripts/Abstract/Enemy_Gunner.cs
+++ Assets/Scripts/Abstract/Enemy_Gunner.cs
@@ -23,16 +23,21 @@ public class Enemy_Gunner : Enemy_Abstract, IShoot
     {
         if(Input.GetMouseButtonDown(0))
         {
-            Debug.Log("撃ったよ");
             Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
             RaycastHit hitInfo;
             if(Physics.Raycast(rayOrigin, out hitInfo))
             {
+                // Interface で機能を管理することで、ダメージを与えることができる敵が多様化しても、短い記述で対応することができる
                 IDamgable obj = hitInfo.collider.GetComponent<IDamgable>();
                 if(obj != null)
                 {
                     obj.DamageHealth(ShootAttackDamage);
                 }
+
+                // if(hitInfo.collider.name == "Enemy_MossGiant")
+                // {
+                //     hitInfo.collider.GetComponent<Enemy_MossGiant>().DamageHealth(ShootAttackDamage);
+                // }
             }
         }
     }
