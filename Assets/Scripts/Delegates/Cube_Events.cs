2ebe9a812c3eca81798c6e4a68dd2f57a2a34468
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 22:09:10 +0900

    Learn Events

---
+++ Assets/Scripts/Delegates/Cube_Events.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cube_Events : MonoBehaviour
+{
+    void Start()
+    {
+        Events.onClick += TurnRed;
+    }
+    public void TurnRed()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+
+    public void OnDisable()
+    {
+        Events.onClick -= TurnRed;
+    }
+}
---
+++ Assets/Scripts/Delegates/Events.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// lear Events : This is a powerful system that calls functions from one class to another without a direct link.
+public class Events : MonoBehaviour
+{
+    public delegate void ActionClick();
+    public static event ActionClick onClick;
+    public void ButtonClick()
+    {
+        // turn all cubes red
+        if(onClick != null)
+        {
+            onClick();
+        }
+    }
+}
