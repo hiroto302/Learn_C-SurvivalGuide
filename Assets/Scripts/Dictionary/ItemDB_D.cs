79f63fbfc7426717d407117c5cf7c728986ec7e0
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 13:28:18 +0900

    How to use foreach to access a Dictionary

--- Assets/Scripts/Dictionary/ItemDB_D.cs
+++ Assets/Scripts/Dictionary/ItemDB_D.cs
@@ -10,12 +10,47 @@ public class ItemDB_D : MonoBehaviour
     private void Start()
     {
         Item_D sword = new Item_D("sword", 0);
+        Item_D bread = new Item_D("bread", 1);
 
-        // itemList.Add(sword);
+        itemList.Add(sword);
+        itemList.Add(bread);
         itemDictionary.Add(0, sword);
+        itemDictionary.Add(1, bread);
 
         var dictionaryItem0 = itemDictionary[0];
-        Debug.Log(dictionaryItem0.name);
-        Debug.Log(dictionaryItem0.id);
+        // Debug.Log(dictionaryItem0.name);
+        // Debug.Log(dictionaryItem0.id);
+
+        for(int i = 0; i < itemList.Count; i++)
+        {
+            // Debug.Log(itemList[i].id);
+            // Debug.Log(itemList[i].name);
+        }
+
+        for(int i = 0; i < itemDictionary.Count; i++)
+        {
+            // Debug.Log(itemDictionary[i].id);
+            // Debug.Log(itemDictionary[i].name);
+        }
+
+
+        /* How to access using foreach */
+
+        // foreach(var item in itemDictionary)
+        foreach(KeyValuePair<int, Item_D> item in itemDictionary)
+        {
+            // Debug.Log(item.Key);
+            // Debug.Log(item.Value.id);
+            // Debug.Log(item.Value.name);
+        }
+
+        foreach(int key in itemDictionary.Keys)
+        {
+            // Debug.Log(key);
+        }
+        foreach(Item_D item in itemDictionary.Values)
+        {
+            // Debug.Log("item name : " + item.name);
+        }
     }
 }
