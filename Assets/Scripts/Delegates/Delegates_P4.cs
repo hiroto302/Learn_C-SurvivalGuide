ec1bf86edc9a8b5d1768341d079f177e7319e85a
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 03:38:46 +0900

    Practicing4

---
+++ Assets/Scripts/Delegates/Delegates_P4.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+// Practicing_4
+public class Delegates_P4 : MonoBehaviour
+{
+    // Func<引数, 引数, 返り値>
+    public Func<int, int, int> onCalculateSum;
+
+    void Start()
+    {
+        // 行が複数になるときは、return Key を記述する必要がある
+        onCalculateSum = (a, b) =>{
+            // a + b;
+            return a + b;
+        };
+        Debug.Log("P4 : " + onCalculateSum(3,4));
+    }
+}
