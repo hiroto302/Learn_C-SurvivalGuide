29f5dd0dacccc0ff96c21f6307a84bfef4d82505
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 22:27:22 +0900

    Event Functions について

--- Assets/Scripts/Delegates/Events.cs
+++ Assets/Scripts/Delegates/Events.cs
@@ -2,7 +2,9 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-// lear Events : This is a powerful system that calls functions from one class to another without a direct link.
+/*  lear Events : This is a powerful system that calls functions from one class to another without a direct link.
+    other class can invoke and control the delegate while you can only subscribe or unsubscribe a method to an event.
+    他のクラスでイベント内容を決めることができるが,イベントを実行制御をするのはこのクラスのみ */
 public class Events : MonoBehaviour
 {
     public delegate void ActionClick();
