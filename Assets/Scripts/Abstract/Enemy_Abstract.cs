2e75d164c55330aceb3b42e972ff6e3e6959f2b1
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 15:46:05 +0900

    Learn AbstractClass
    bace.virtual Method
    throw new System.NotImplementedException()

---
+++ Assets/Scripts/Abstract/Enemy_Abstract.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class Enemy_Abstract : MonoBehaviour
+{
+    public int speed;
+    public int health;
+    public int gems;
+
+    public abstract void Attack();
+
+    public virtual void Die()
+    {
+        Destroy(this.gameObject);
+    }
+}
+
---
+++ Assets/Scripts/Abstract/Enemy_MossGiant.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_MossGiant : Enemy_Abstract
+{
+
+    void Start()
+    {
+        Attack();
+        Die();
+    }
+    public override void Attack()
+    {
+
+        // コンソールに表示してくれる例外処理 ： NotImplementedException: The method or operation is not implemented.
+        // throw new System.NotImplementedException();
+        Debug.Log("攻撃");
+    }
+
+    // 全て書き換えたり、共通機能を扱いつつ、処理を追加することもできる
+    public override void Die()
+    {
+        base.Die();
+        // 追加処理
+        Debug.Log("やられたよ");
+    }
+}
