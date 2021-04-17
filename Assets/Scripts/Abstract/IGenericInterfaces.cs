5f91a1de674d69929b88f978efde4c6aaeca32ab
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 18:07:42 +0900

    Learn GenericInterface

---
+++ Assets/Scripts/Abstract/IGenericInterfaces.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public interface IGenericInterfaces<T>
+{
+    void GenericMethod(T type);
+
+    // void GenericMethod(int i);
+    // void GenericMethod(float f);
+    // void GenericMethod(GameObject obj);
+}
---
+++ Assets/Scripts/Abstract/Slime_GenericType.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Slime_GenericType : MonoBehaviour, IGenericInterfaces<Vector3>
+{
+    void Start()
+    {
+        // GenericMethod(1.2f);
+        GenericMethod(new Vector3(1.5f, 1.5f, 1.5f));
+    }
+    // public void GenericMethod(float type)
+    // {
+    //     transform.localScale = new Vector3(type, type, type);
+    // }
+    public void GenericMethod(Vector3 type)
+    {
+        transform.localScale = type;
+    }
+}
