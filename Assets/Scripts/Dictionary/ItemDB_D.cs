f3e014a12b6c872b43715147740cac003ab523ac
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 13:03:49 +0900

    Dictionaryを利用した管理方法について

---
+++ Assets/Scripts/Dictionary/ItemDB_D.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemDB_D : MonoBehaviour
+{
+    public List<Item_D> itemList = new List<Item_D>();
+    public Dictionary<int, Item_D> itemDictionary = new Dictionary<int, Item_D>();
+
+    private void Start()
+    {
+        Item_D sword = new Item_D("sword", 0);
+
+        // itemList.Add(sword);
+        itemDictionary.Add(0, sword);
+
+        var dictionaryItem0 = itemDictionary[0];
+        Debug.Log(dictionaryItem0.name);
+        Debug.Log(dictionaryItem0.id);
+    }
+}
---
+++ Assets/Scripts/Dictionary/Item_D.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Item_D
+    {
+    public string name;
+    public int id;
+
+    public Item_D(string name, int id)
+    {
+        this.name = name;
+        this.id = id;
+    }
+}
