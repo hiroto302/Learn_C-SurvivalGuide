42f6ab5463de85f142f7a4aaf08d479f85d3ddbb
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 22:08:32 +0900

    Learn Delegate

---
+++ Assets/Scripts/Delegates/Delegates.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+// Learn Delegate
+public class Delegates : MonoBehaviour
+{
+    // Declare delegate
+    public delegate void ChangeColor(Color newColor);
+    // delegate Variable : Reference Type
+    public ChangeColor onColorChange;
+
+    public delegate void OnComplete();
+    public OnComplete onComplete;
+
+    void Start()
+    {
+        onColorChange = UpdateColor;
+        onColorChange(Color.green);
+
+        // Debug.Log(onComplete) = null;
+        onComplete = Task;
+        if(onComplete != null)
+        {
+            onComplete();
+            onComplete -= Task;
+            onComplete = onComplete + Task2 + Task3;
+            onComplete();
+        }
+    }
+
+    public void UpdateColor(Color newColor)
+    {
+        Debug.Log("Change Color to " + newColor.ToString());
+    }
+    public void Task()
+    {
+        Debug.Log("Task Finished");
+    }
+    public void Task2()
+    {
+        Debug.Log("Task2 Finished");
+    }
+    public void Task3()
+    {
+        Debug.Log("Task3 Finished");
+    }
+}
