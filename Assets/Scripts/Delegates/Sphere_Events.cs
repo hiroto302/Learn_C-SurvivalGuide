ee2c3e68a4b3dc188e0c1f511f69db15ef4f9434
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-17 22:34:13 +0900

    各クラスのイベントメソッドの実行例

---
+++ Assets/Scripts/Delegates/Sphere_Events.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sphere_Events : MonoBehaviour
+{
+
+    void Start()
+    {
+        Events.onClick += Fall;
+    }
+    public void Fall()
+    {
+        GetComponent<Rigidbody>().useGravity = true;
+    }
+}
