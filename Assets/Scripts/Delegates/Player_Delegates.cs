0a5b9d1f672b6a50813bc825934aaf16581b7b98
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 01:06:40 +0900

    Learn System.Action

--- Assets/Scripts/Delegates/Player_Delegates.cs
+++ Assets/Scripts/Delegates/Player_Delegates.cs
@@ -1,17 +1,51 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Player_Delegates : MonoBehaviour
 {
+
+    // public delegate void OnDamageReceived(int currentHealth);
+    // public static event OnDamageReceived onDamage;
+
+    // System.Action を使うことで、上記の２行を下記の１行で済む
+    public static Action<int> OnDamageReceived;
+
     public delegate void OnDeath();
     public static event OnDeath onDeath;
+
+    public static int Health { get; set; }
+
+    void Start()
+    {
+        Health = 10;
+    }
 
     void Update()
     {
         if(Input.GetKeyDown(KeyCode.D))
         {
+            Damage();
+            // Death();
+        }
+    }
+
+    void Damage()
+    {
+        Health--;
+        // if(onDamage != null)
+        // {
+        //     onDamage();
+        // }
+        if(OnDamageReceived != null)
+        {
+            OnDamageReceived(Health);
+        }
+        if(Health <= 0)
+        {
             Death();
+            Health = 10;
         }
     }
 
--- Assets/Scripts/Delegates/UIManager.cs
+++ Assets/Scripts/Delegates/UIManager.cs
@@ -6,11 +6,32 @@ using UnityEngine.UI;
 public class UIManager : MonoBehaviour
 {
     public int deathCount;
+    public Text currnetHealthText;
     public Text deathCountText;
 
     public void OnEnable()
     {
+        Player_Delegates.OnDamageReceived += UpdateHealth;
         Player_Delegates.onDeath += UpdateDeathCount;
+    }
+
+    void Start()
+    {
+        Player_Delegates.OnDamageReceived(Player_Delegates.Health);
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.F))
+        {
+            Player_Delegates.Health--;
+            Player_Delegates.OnDamageReceived(Player_Delegates.Health);
+        }
+    }
+
+    public void UpdateHealth(int health)
+    {
+        // display updated heath here
+        currnetHealthText.text = "Health : " + health.ToString();
     }
 
     public void UpdateDeathCount()
