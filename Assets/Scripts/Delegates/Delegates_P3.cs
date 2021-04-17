4621871319996035eb0a8f5dc0cbd64f0b3ee01b
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 03:24:11 +0900

    Practicing3

---
+++ Assets/Scripts/Delegates/Delegates_P3.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using UnityEngine;
+
+// Practicing_3
+public class Delegates_P3 : MonoBehaviour
+{
+    public Func<int> onGetName;
+
+    public void Start()
+    {
+        onGetName = () => this.gameObject.name.Length;
+        int characterCount = onGetName();
+        Debug.Log("P3 : " + characterCount.ToString());
+    }
+}
