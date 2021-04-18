dfaefc751fa43259ee790118d986e866291aa8f0
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 19:00:49 +0900

    Learn Singleton Design Pattern

---
+++ Assets/Scripts/Singleton/GameManager_Singleton.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager_Singleton : MonoBehaviour
+{
+    private static GameManager_Singleton _instance;
+    public static GameManager_Singleton Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                Debug.Log("The GameManager is NULL");
+            }
+            return _instance;
+        }
+    }
+
+    private void Awake()
+    {
+        _instance = this;
+    }
+
+    public void DisplayMyName()
+    {
+        Debug.Log("My name is T.H.");
+    }
+}
---
+++ Assets/Scripts/Singleton/Player_Singleton.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player_Singleton : MonoBehaviour
+{
+    public void Start()
+    {
+        // Simple Accese to Singleton
+        GameManager_Singleton.Instance.DisplayMyName();
+    }
+}
