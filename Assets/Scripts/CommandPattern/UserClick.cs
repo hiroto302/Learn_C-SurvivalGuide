96875734568eda8fd162e1abe7f7d3c8c8e407b3
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-19 01:22:12 +0900

    Learn_Command Pattern - Setup and Implementation

---
+++ Assets/Scripts/CommandPattern/UserClick.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UserClick : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray rayOrigin = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hitInfo;
+            if(Physics.Raycast(rayOrigin, out hitInfo))
+            {
+                if (hitInfo.collider.tag == "Cube")
+                {
+                    // execute click command
+                    ICommand click = new ClickCommand(hitInfo.collider.gameObject , new Color(Random.value, Random.value, Random.value));
+                    click.Execute();
+
+                    // hitInfo.collider.GetComponent<MeshRenderer>().material.color = new Color(
+                    //     Random.value, Random.value, Random.value);
+                }
+            }
+        }
+    }
+}
+
