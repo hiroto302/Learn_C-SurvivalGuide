1bc3898953ed3eea9a0e085b822bfac3ce0730a7
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 15:06:19 +0900

    Linq_Distinct

--- Assets/Scripts/LINQ/LinqExample.cs
+++ Assets/Scripts/LINQ/LinqExample.cs
@@ -5,17 +5,25 @@ using System.Linq;
 
 public class LinqExample : MonoBehaviour
 {
-    string[] names = {"a", "b", "c", "d", "e"};
+    string[] names = {"a", "a", "b", "c", "d", "e", "e"};
 
     void Start()
     {
         // Any
         var nameFound = names.Any(name => name == "c");
-        Debug.Log("nameFound : " + nameFound);
+        // Debug.Log("nameFound : " + nameFound);
 
         // Contains
         var nameContain = name.Contains("d");
-        Debug.Log("nameContains : " + nameContain);
+        // Debug.Log("nameContains : " + nameContain);
+
+        // Distinct
+        var uniqueNames = names.Distinct();
+        foreach(var name in uniqueNames)
+        {
+            Debug.Log(name);
+        }
+
 
         // foreach (var name in names)
         // {
