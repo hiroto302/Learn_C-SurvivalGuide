a81cb5dd6cb92b46adb74512a761700663da6dd2
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 23:22:42 +0900

    Learn Object Pooling Design Pattern

---
+++ Assets/Scripts/Pool/PoolManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PoolManager : MonoBehaviour
+{
+    // turn this class into a singleton for easy accessibility
+    private static PoolManager _instance;
+    public static PoolManager Instance
+    {
+        get
+        {
+            if(_instance == null)
+                Debug.LogError("The PoolManager is NULL");
+
+            return _instance;
+        }
+    }
+
+    [SerializeField]
+    private GameObject _bulletContainer;
+    [SerializeField]
+    private GameObject _bulletPrefab;
+    [SerializeField]
+    private List<GameObject> _bulletPool;
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    void Start()
+    {
+        _bulletPool = GenerateBullets(10);
+    }
+
+    // pregenerate a list of bullets using the bullet prefab
+    List<GameObject> GenerateBullets(int amountOfBullets)
+    {
+        for(int i = 0; i < amountOfBullets; i++)
+        {
+            GameObject bullet = Instantiate(_bulletPrefab);
+            bullet.transform.parent = _bulletContainer.transform;
+            bullet.SetActive(false);
+
+            _bulletPool.Add(bullet);
+        }
+        return _bulletPool;
+    }
+}
---
+++ Assets/Scripts/Pool/bullet.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bullet : MonoBehaviour
+{
+    void Start()
+    {
+        // この記述ではパフォーマンスが悪い (batches が増加する)
+        Destroy(this.gameObject, 1.0f);
+    }
+
+    void Update()
+    {
+        
+    }
+}
