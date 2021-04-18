cb0ed2f35a5722e4fefdeec5181f91c5ac81d221
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 20:38:50 +0900

    Singleton_How to lazily instantiate an object

--- Assets/Scripts/Singleton/Player_Singleton.cs
+++ Assets/Scripts/Singleton/Player_Singleton.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Player_Singleton : MonoBehaviour
 {
+    UIManager_Singleton ui;
     public void Start()
     {
         // Simple Accese to Singleton
         GameManager_Singleton.Instance.DisplayMyName();
+        ui = UIManager_Singleton.Instance;
+        ui.UpdateHealth();
+        ui.UpdateScore();
+        SpawnManager_Singleton.Instance.SpawnGameObject();
     }
 }
---
+++ Assets/Scripts/Singleton/SpawnManager_Singleton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager_Singleton : MonoBehaviour
+{
+    private static SpawnManager_Singleton _instance;
+    public static SpawnManager_Singleton Instance
+    {
+        get
+        {
+            if(_instance == null)
+            {
+                GameObject go = new GameObject("Spawn_Manager");
+                go.AddComponent<SpawnManager_Singleton>();
+            }
+            return _instance;
+        }
+    }
+
+    void Awake()
+    {
+        _instance = this;
+    }
+
+    public void SpawnGameObject()
+    {
+        Debug.Log("to lazily instantiate an object !!");
+    }
+}
