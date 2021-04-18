d6b4b3aac687fe9a7ddf899e2c92d6eb5264ddac
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 14:48:29 +0900

    Linq_Any

---
+++ Assets/Scripts/LINQ/LinqExample.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class LinqExample : MonoBehaviour
+{
+    string[] names = {"a", "b", "c", "d", "e"};
+
+    void Start()
+    {
+        // Any
+        var nameFound = names.Any(name => name == "c");
+        Debug.Log("nameFound : " + nameFound);
+
+        //
+
+        // foreach (var name in names)
+        // {
+        //     if(name == "c")
+        //     {
+        //         Debug.Log("match !! : " + name);
+        //     }
+        // }
+    }
+}
