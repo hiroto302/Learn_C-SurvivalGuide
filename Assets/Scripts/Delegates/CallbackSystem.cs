979eb53150ce01dc64d244bd35803e6db7836e45
hiroto302 <i.am.a.hiroto@gmail.com>
2021-04-18 14:15:40 +0900

    Simple Call Back System

---
+++ Assets/Scripts/Delegates/CallbackSystem.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class CallbackSystem : MonoBehaviour
+{
+    public void Start()
+    {
+        StartCoroutine(MyRoutine(() => {
+            Debug.Log("The routine has finished");
+        }));
+    }
+
+    public IEnumerator MyRoutine(Action onComplete = null)
+    {
+        yield return new WaitForSeconds(5.0f);
+        if(onComplete != null)
+        {
+            onComplete();
+        }
+    }
+}
